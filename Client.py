d40a90c89b56d3a92c8461a9932bf81055111696
samuelshokrywahba <84352026+samuelshokrywahba@users.noreply.github.com>
2021-05-20 13:48:04 +0200

    Add files via upload

---
+++ Client.py
@@ -0,0 +1,19 @@
+from socket import *
+from _thread import *
+import threading
+
+socket_file_discriptor = socket(AF_INET, SOCK_STREAM)
+host = "127.0.0.1"
+port = 7000
+
+def recieved_thread(c):
+    while True:
+        x=c.recv(500)
+        print(x.decode("utf=8"))
+socket_file_discriptor.connect((host, port))
+start_new_thread(recieved_thread, (socket_file_discriptor,))
+
+while True:
+    socket_file_discriptor.send(input("client>> ").encode("utf=8"))
+
+
